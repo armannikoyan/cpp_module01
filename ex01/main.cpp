c7a5c00843ce9ed9fa08f78db35891b1268907a8
Arman Nikoyan <armannikoyan@Armans-MacBook-Air.local>
2025-04-27 03:12:47 +0400

    done: ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 18:41:07 by anikoyan          #+#    #+#             */
-/*   Updated: 2025/04/22 19:53:35 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/04/27 02:58:51 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,11 @@
 
 int main()
 {
-  Zombie *my_zombie = newZombie("Po");
+  const int N = 10;
+  Zombie *my_zombies = zombieHorde(N, "Arman");
 
-  my_zombie->announce();
-  randomChump("Shifu");
-  delete my_zombie;
+  for (int i = 0; i <= N; ++i)
+    my_zombies[i].announce();
+  delete[] my_zombies;
   return 0;
 }
--- ex01/zombieHorde.cpp
+++ ex01/zombieHorde.cpp
@@ -6,7 +6,7 @@
 /*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/22 20:15:22 by anikoyan          #+#    #+#             */
-/*   Updated: 2025/04/24 12:39:11 by anikoyan         ###   ########.fr       */
+/*   Updated: 2025/04/27 02:51:59 by anikoyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,18 @@
 
 Zombie* zombieHorde(int N, std::string name)
 {
+  if (N < 1)
+  {
+    std::cout << "A Horde can not contain less than 1 Zombie." << std::endl;
+    return (nullptr);
+  }
+  Zombie *horde = new Zombie[N];
+  if (horde == nullptr)
+  {
+    std::cout << "Allocation of the horde failed." << std::endl;
+    return (nullptr);
+  }
+  for (int i = 0; i < N; ++i)
+    horde[i].set_name(name);
+  return horde;
 }
