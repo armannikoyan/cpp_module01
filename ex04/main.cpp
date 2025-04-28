c61d2f17b7ecea61a9c26eef3bfbfd7e7a2f0b11
Arman Nikoyan <armannikoyan@Armans-MacBook-Air.local>
2025-04-28 15:51:00 +0400

    done: ex04

---
+++ ex04/main.cpp
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: anikoyan <anikoyan@student.42yerevan.am>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/28 09:55:43 by anikoyan          #+#    #+#             */
+/*   Updated: 2025/04/28 15:49:55 by anikoyan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <fstream>
+#include <ios>
+#include <string>
+
+int main(int argc, char **argv)
+{
+  if (argc != 3 && argc != 4)
+  {
+    std::cerr << "Usage: " << argv[0] << " filename replaceable replacement[can be ommited to replace with nothing]\n";
+    return EXIT_FAILURE;
+  }
+
+  std::ifstream input_file(argv[1]);
+  if (!input_file)
+  {
+    std::cerr << "Error: Could not open input file.\n";
+    return EXIT_FAILURE;
+  }
+
+  std::ofstream output_file(std::string(argv[1]) + ".replace");
+  if (!output_file)
+  {
+    std::cerr << "Error: Could not create output file.\n";
+    return EXIT_FAILURE;
+  }
+
+  std::string replaceable = argv[2];
+  std::string replacement = argv[3];
+
+  if (replaceable.empty())
+  {
+    std::cerr << "Error: Replaceable string cannot be empty.\n";
+    return EXIT_FAILURE;
+  }
+
+  std::string content(
+      (std::istream_iterator<char>(input_file))
+      , std::istream_iterator<char>()
+      );
+  input_file.close();
+
+  std::string result;
+  size_t start_pos = 0;
+  size_t found_pos = 0;
+
+  while ((found_pos = content.find(replaceable, start_pos)) != std::string::npos)
+  {
+    result.append(content, start_pos, found_pos - start_pos);
+    result += replacement;
+    start_pos = found_pos + replacement.length();
+    if (found_pos == std::string::npos) break;
+  }
+  result.append(content, start_pos, std::string::npos);
+
+  output_file << result;
+  output_file.close();
+
+  return EXIT_SUCCESS;
+}
